f3d8dcb454ef64235ea0e323fddb1525ca8f87b2
dgj7 <garciad48879@gmail.com>
2025-12-21 14:39:46 -0500

    game,engine: externalize grid drawing code

--- engine/src/render/graphics/opengl/opengl_wrapper.rs
+++ engine/src/render/graphics/opengl/opengl_wrapper.rs
@@ -2,7 +2,9 @@ use crate::math::twod::line_2d::Line2D;
 use crate::render::graphics::opengl::opengl_api::{gl_begin_lines, gl_clear, gl_clear_color, gl_color_3f, gl_end, gl_flush, gl_line_width, gl_load_identity, gl_matrix_mode, gl_ortho, gl_vertex_2f, gl_viewport};
 use crate::render::model::color::Color;
 use windows::Win32::Graphics::OpenGL::{GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT, GL_PROJECTION};
+use crate::math::twod::dimension_2d::Dimension2D;
 use crate::math::twod::draw_config_2d::DrawingConfig2D;
+use crate::math::twod::point_2d::Point2D;
 use crate::render::model::render_context::RendererContext;
 
 pub fn paint_background(color: Color) {
@@ -36,4 +38,30 @@ pub fn paint_lines(lines: &[Line2D], config: &DrawingConfig2D) {
     gl_end();
     
     gl_flush();
+}
+
+pub fn paint_grid(client: &Dimension2D) {
+    /* draw x&y axes in white */
+    paint_lines(&*vec!(
+        Line2D::new(Point2D::origin(), Point2D::new(0.0, client.height)),
+        Line2D::new(Point2D::origin(), Point2D::new(client.width, 0.0)),
+    ), &DrawingConfig2D::new(Color::from_rgb(0.498, 0.0, 1.0), 10.0));
+
+    /* draw x-axis horizontal lines */
+    let hgap = 10;
+    let hiters = ((client.height + (hgap as f32))/(hgap as f32)) as u16;
+    let mut hlines: Vec<Line2D> = Vec::with_capacity((hiters + 10) as usize);
+    for h in 0..hiters {
+        hlines.push(Line2D::new(Point2D::new(0.0, (h * hgap) as f32), Point2D::new(client.width, (h * hgap) as f32)));
+    }
+    paint_lines(&*hlines, &DrawingConfig2D::new(Color::from_rgb(0.2, 0.2, 0.2), 1.0));
+
+    /* draw y-axis vertical lines */
+    let vgap = 10;
+    let viters = ((client.width + (vgap as f32))/(vgap as f32)) as u16;
+    let mut vlines: Vec<Line2D> = Vec::with_capacity((viters + 10) as usize);
+    for v in 0..viters {
+        vlines.push(Line2D::new(Point2D::new((v * vgap) as f32, 0.0), Point2D::new((v * vgap) as f32, client.height)));
+    }
+    paint_lines(&*vlines, &DrawingConfig2D::new(Color::from_rgb(0.2, 0.2, 0.2), 1.0));
 }
--- game/src/impls/game_renderer.rs
+++ game/src/impls/game_renderer.rs
@@ -4,7 +4,7 @@ use engine::logger::log_level::LogLevel;
 use engine::math::twod::draw_config_2d::DrawingConfig2D;
 use engine::math::twod::line_2d::Line2D;
 use engine::math::twod::point_2d::Point2D;
-use engine::render::graphics::opengl::opengl_wrapper::{paint_background, paint_lines, prepare_2d};
+use engine::render::graphics::opengl::opengl_wrapper::{paint_background, paint_grid, paint_lines, prepare_2d};
 use engine::render::model::color::Color;
 use engine::render::model::render_context::RendererContext;
 use engine::render::renderer::Renderer;
@@ -50,33 +50,10 @@ impl Renderer for GameRenderer {
 
         /* prepare for 2d drawing */
         prepare_2d(context);
-
-        /* draw black background */
         paint_background(Color::BLACK);
 
-        /* draw x&y axes in white */
-        paint_lines(&*vec!(
-            Line2D::new(Point2D::origin(), Point2D::new(0.0, ccd.height)),
-            Line2D::new(Point2D::origin(), Point2D::new(ccd.width, 0.0)),
-        ), &DrawingConfig2D::new(Color::from_rgb(0.498, 0.0, 1.0), 10.0));
-
-        /* draw x-axis horizontal lines */
-        let hgap = 10;
-        let hiters = ((ccd.height + (hgap as f32))/(hgap as f32)) as u16;
-        let mut hlines: Vec<Line2D> = Vec::with_capacity((hiters + 10) as usize);
-        for h in 0..hiters {
-            hlines.push(Line2D::new(Point2D::new(0.0, (h * hgap) as f32), Point2D::new(ccd.width, (h * hgap) as f32)));
-        }
-        paint_lines(&*hlines, &DrawingConfig2D::new(Color::from_rgb(0.2, 0.2, 0.2), 1.0));
-
-        /* draw y-axis vertical lines */
-        let vgap = 10;
-        let viters = ((ccd.width + (vgap as f32))/(vgap as f32)) as u16;
-        let mut vlines: Vec<Line2D> = Vec::with_capacity((viters + 10) as usize);
-        for v in 0..viters {
-            vlines.push(Line2D::new(Point2D::new((v * vgap) as f32, 0.0), Point2D::new((v * vgap) as f32, ccd.height)));
-        }
-        paint_lines(&*vlines, &DrawingConfig2D::new(Color::from_rgb(0.2, 0.2, 0.2), 1.0));
+        /* paint the full-screen grid */
+        paint_grid(&ccd);
     }
 
     fn render_3d_scene(&self, _context: &mut RendererContext) {
