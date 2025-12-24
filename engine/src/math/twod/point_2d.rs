f52ab836d9e51c5fb43741c738a87831927db115
dgj7 <garciad48879@gmail.com>
2025-12-23 22:15:49 -0500

    engine: distance function, 2d

--- engine/src/math/twod/point_2d.rs
+++ engine/src/math/twod/point_2d.rs
@@ -1,16 +1,21 @@
-use num_traits::Num;
+use num_traits::Float;
+use std::ops::{Add, Sub};
 
-pub struct Point2D<F: Num> {
+pub struct Point2D<F: Float + Sub<F> + Add<F>> {
     pub x: F,
     pub y: F,
 }
 
-impl<F: Num> Point2D<F> {
+impl<F: Float> Point2D<F> {
     pub fn new(x: F, y: F) -> Point2D<F> {
         Point2D { x, y }
     }
 
     pub fn origin() -> Point2D<F> {
         Point2D { x: F::zero(), y: F::zero() }
+    }
+
+    pub fn distance(&self, other: &Point2D<F>) -> F {
+        ((self.x-other.x).powi(2) + (self.y-other.y).powi(2)).sqrt()
     }
 }
