31a8ccdd51c5aec3cc8223d596a143d82ac201c3
dgj7 <garciad48879@gmail.com>
2025-12-30 17:09:01 -0500

    engine: simplify subsystem render calls

--- engine/src/graphics.rs
+++ engine/src/graphics.rs
@@ -23,36 +23,32 @@ impl<F: Float + Add<F> + Sub<F>> GraphicsIntermediary<F> {
             subsystem: grss_factory(grss),
         }
     }
-    
+
     pub(crate) fn initialize(&self) {
         self.subsystem.initialize();
     }
-    
+
     pub(crate) fn before_scene(&self, ccd: &Dimension2D<f32>) {
         self.subsystem.before_scene(ccd);
     }
-    
+
     pub(crate) fn prepare_2d(&self, ccd: &Dimension2D<f32>) {
         self.subsystem.prepare_2d(ccd);
     }
-    
+
     pub(crate) fn render_2d(&self, g2d: &Graph2D<F>) {
         for (_, model) in g2d.models.iter() {
-            for lines in &model.lines {
-                self.subsystem.render_2d_lines(lines);
-            }
+            model.lines.iter().for_each(|x| self.subsystem.render_2d_lines(x));
         }
     }
-    
+
     pub(crate) fn prepare_3d(&self) {
         self.subsystem.prepare_3d();
     }
-    
+
     pub(crate) fn render_3d(&self, g3d: &Graph3D<F>) {
         for (_, model) in g3d.models.iter() {
-            for lines in &model.lines {
-                self.subsystem.render_3d_lines(lines);
-            }
+            model.lines.iter().for_each(|x| self.subsystem.render_3d_lines(x));
         }
     }
 }
