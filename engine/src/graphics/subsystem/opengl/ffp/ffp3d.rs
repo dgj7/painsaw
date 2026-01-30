da5ed42fb0f0ab8228af119bc0a89161f475263f
dgj7 <garciad48879@gmail.com>
2026-01-29 19:49:49 -0500

    engine: introduce translations

--- engine/src/graphics/subsystem/opengl/ffp/ffp3d.rs
+++ engine/src/graphics/subsystem/opengl/ffp/ffp3d.rs
@@ -1,9 +1,10 @@
 use crate::config::{CVAR_FOV, DEFAULT_FOV};
 use crate::graphics::geometry::primitive::prim3d::Primitive3D;
-use crate::graphics::subsystem::opengl::ffp::api::{gl_begin_lines, gl_begin_points, gl_color_3f, gl_enable, gl_end, gl_line_width, gl_load_identity, gl_matrix_mode, gl_point_size, gl_pop_attrib, gl_pop_matrix, gl_push_attrib, gl_push_matrix, gl_rotate_f, gl_translate_f, gl_vertex_3f, glu_perspective};
+use crate::graphics::subsystem::opengl::ffp::api::{gl_begin_lines, gl_begin_points, gl_color_3f, gl_enable, gl_end, gl_line_width, gl_load_identity, gl_matrix_mode, gl_point_size, gl_pop_attrib, gl_pop_matrix, gl_push_attrib, gl_push_matrix, gl_rotate_f, gl_scale_f, gl_translate_f, gl_vertex_3f, glu_perspective};
 use crate::window::context::RendererContext;
 use num_traits::Float;
 use windows::Win32::Graphics::OpenGL::{GL_ALL_ATTRIB_BITS, GL_DEPTH_TEST, GL_MODELVIEW, GL_PROJECTION};
+use crate::graphics::geometry::orient::Orientation;
 
 pub(crate) fn ffp_3d_setup<F: Float>(context: &RendererContext<F>) {
     /* save prior state before 3d rendering */
@@ -46,11 +47,21 @@ pub(crate) fn ffp_3d_teardown() {
     gl_pop_matrix();
 }
 
+fn ffp_3d_translate<F: Float>(orientation: &Orientation<F>) {
+    let position = orientation.position.column_major_position();
+    let yaw = orientation.yaw().to_f32().unwrap();
+    let pitch = orientation.pitch().to_f32().unwrap();
+    gl_translate_f(position.x.to_f32().unwrap(), position.y.to_f32().unwrap(), position.z.to_f32().unwrap());
+    gl_rotate_f(pitch, 1.0, 0.0, 0.0);
+    gl_rotate_f(yaw, 0.0, 1.0, 0.0);
+    gl_scale_f(orientation.x_scale.to_f32().unwrap(), orientation.y_scale.to_f32().unwrap(), orientation.z_scale.to_f32().unwrap());
+}
+
 pub(crate) fn ffp_3d_points<F: Float>(primitive: &Primitive3D<F>, point_size: F) {
     gl_push_matrix();
     gl_push_attrib(GL_ALL_ATTRIB_BITS);
 
-    // todo: translate, rotate, scale
+    ffp_3d_translate(&primitive.orientation);
 
     gl_color_3f(primitive.color.red, primitive.color.green, primitive.color.blue);
     gl_point_size(point_size.to_f32().unwrap());
@@ -69,7 +80,7 @@ pub(crate) fn ffp_3d_lines<F: Float>(primitive: &Primitive3D<F>, thickness: F) {
     gl_push_matrix();
     gl_push_attrib(GL_ALL_ATTRIB_BITS);
 
-    // todo: translate, rotate, scale
+    ffp_3d_translate(&primitive.orientation);
 
     gl_color_3f(primitive.color.red, primitive.color.green, primitive.color.blue);
     gl_line_width(thickness.to_f32().unwrap());
