95b5fc9b590bf3373e1b19c6d1496a052a6e6237
dgj7 <garciad48879@gmail.com>
2026-02-12 19:53:24 -0500

    demo1: silence mouse move logger

--- demo1/src/mouse_inputs.rs
+++ demo1/src/mouse_inputs.rs
@@ -1,12 +1,10 @@
 use engine::config::input_config::mc::MouseHandler;
-use engine::logger::log;
-use engine::logger::log_level::LogLevel;
 use engine::window::context::RendererContext;
 
 pub(crate) struct MouseInputs {}
 
 impl MouseHandler for MouseInputs {
-    fn handle_mouse_move(&self, x: i32, y: i32, _context: &mut RendererContext) {
-        log(LogLevel::Debug, &|| String::from(format!("mouse-move: ({},{})", x, y)));
+    fn handle_mouse_move(&self, _x: i32, _y: i32, _context: &mut RendererContext) {
+        //log(LogLevel::Debug, &|| String::from(format!("mouse-move: ({},{})", x, y)));
     }
 }
