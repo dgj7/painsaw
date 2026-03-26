933297b220033afe589f8a4d8968fa1fbab346e5
dgj7 <garciad48879@gmail.com>
2026-03-25 21:16:42 -0400

    d2: compile texture directly into demo program

--- d2/src/d2wc.rs
+++ d2/src/d2wc.rs
@@ -1,6 +1,8 @@
 use engine::window::context::RendererContext;
 use engine::window::wc::WorldController;
 
+const B24_BMP: &[u8] = include_bytes!("../assets/24b.bmp");
+
 pub(crate) struct Demo2WorldController {}
 
 impl WorldController for Demo2WorldController {
