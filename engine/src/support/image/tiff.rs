26e8fe8f5146fd1fbed2ee9f1074dd66020c4e7c
dgj7 <garciad48879@gmail.com>
2026-05-02 12:09:03 -0400

    engine: make tiff image-compliant

--- engine/src/support/image/tiff.rs
+++ engine/src/support/image/tiff.rs
@@ -1,9 +1,13 @@
-use std::path::Path;
-use crate::support::image::RawImage;
+use crate::support::image::{Image, RawImage};
+use std::io::{BufRead, Seek};
+
+pub struct Tiff;
 
 ///
 /// load tiff image files.
 ///
-pub fn load_tiff<P: AsRef<Path>>(_path: P) -> std::io::Result<RawImage> {
-    todo!("implement tiff loading")
+impl Image for Tiff {
+    fn load_from_buf_read<R: BufRead + Seek>(_reader: R) -> std::io::Result<RawImage> {
+        todo!()
+    }
 }
